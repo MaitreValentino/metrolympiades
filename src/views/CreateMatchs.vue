ddd64d1d97ed9569c8d31db37e4f3a9793cb0b3b
22013679 <mattheo.mouchel@gmail.com>
2024-03-29 09:45:43 +0100

    add : Some CSS

--- src/views/CreateMatchs.vue
+++ src/views/CreateMatchs.vue
@@ -36,31 +36,34 @@ onMounted(() => {
     getSport()
 })
 
+
 </script>
 
 
 <template>
-    <div>
+    <div class="flex flex-col items-center gap-8 p-20">
         
-        <P>Matchs</P>
+        <h1 class="text-3xl text-blue-600">Matchs</h1>
 
-        <select v-model="team1model" name="team1" id="team1-select">
+        <label> Team 1:<select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="team1model" name="team1" id="team1-select">
         <option value="">Team 1 :</option>
         <option v-for="item in team" :value=item.id>{{ item.name }}</option>
+        </select></label>
+        <label>Team 2:
+            <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="team2model" name="team2" id="team2-select">
+            <option value="">Team 2 :</option>
+            <option v-for="item in team" :value=item.id>{{ item.name }}</option>
         </select>
-        <br>
-        <select v-model="team2model" name="team2" id="team2-select">
-        <option value="">Team 2 :</option>
-        <option v-for="item in team" :value=item.id>{{ item.name }}</option>
-        </select>
-        <br>
-        <select v-model="sportmodel" name="sports" id="sport-select">
+        </label>
+        <label>Sport:
+        <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="sportmodel" name="sports" id="sport-select">
         <option value="">Sports :</option>
         <option v-for="item in sports" :value="item.id">{{ item.intitule }}</option>
         </select>
+        </label>
         <br>
-        <label>Time : </label><input type="time" id="heurematch" name="heurematch" min="09:00" max="22:00"/>
+        <label>Time : <input type="time" id="heurematch" name="heurematch" min="09:00" max="22:00"/> </label>
         <br>
-        <button class="border-2 rounded-full" @click="submit">Create a match</button>
+        <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" @click="submit">Create a match</button>
     </div>
 </template>
