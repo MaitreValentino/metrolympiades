0e6495ebb5202bf7e65698cd5120074c412da975
MaitreValentino <valentinsnith@hotmail.com>
2024-03-29 09:53:21 +0100

    Feat: Button to add or delete a member

--- src/views/TeamManager.vue
+++ src/views/TeamManager.vue
@@ -4,18 +4,8 @@ import useSupabase from '@/api/supabase'
 const { supabase } = useSupabase()
 
 var teamName = ref()
-const oldName = teamName
 
-const tabMember = ['Mattheo', 'Raphaël', 'Jean', 'Valentin', 'Arthur']
-
-async function saveTeam() {
-  const { data, error } = await supabase
-    .from('teams')
-    .update({ name: teamName.value })
-    .eq('name', oldName)
-    .select()
-  console.log('Réussi')
-}
+const tabMember = ref(['Mattheo', 'Raphaël', 'Jean', 'Valentin', 'Pric'])
 
 async function getTeamName() {
   const { data: teams } = await supabase.from('teams').select('name')
@@ -26,14 +16,25 @@ getTeamName()
 </script>
 
 <template>
-  <div class="flex flex-col items-center gap-9 p-16">
+  <div class="flex flex-col items-center gap-9 p-5">
     <input class="text-4xl text-center" type="text" :value="teamName" placeholder="Nom d'équipe" />
   </div>
 
   <div class="flex flex-col">
     <h2 class="pb-9 pl-1 text-xl">Members</h2>
     <div class="pb-5 pl-4" v-for="member in tabMember" :key="member">
-      <input type="text" :value="member" />
+      <input type="text" class="border border-black mr-2" :value="member" />
+      <button
+        v-if="tabMember == null"
+        class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800"
+      >
+        +
+      </button>
+      <button
+        class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800"
+      >
+        -
+      </button>
     </div>
   </div>
   <div class="flex flex-row justify-center">
