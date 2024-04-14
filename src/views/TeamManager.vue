91732514fef876411be9c8143d2f2792f14f71ae
Valentin SNITH <valentin.snith@etu.unicaen.fr>
2024-04-14 15:43:59 +0200

    Feat: Leader can add or delete a member and save it

--- src/views/TeamManager.vue
+++ src/views/TeamManager.vue
@@ -1,18 +1,53 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import useSupabase from '@/api/supabase'
 const { supabase } = useSupabase()
 
 var teamName = ref()
 
-const tabMember = ref(['Mattheo', 'Raphaël', 'Jean', 'Valentin', 'Pric'])
+const name = ref()
+
+const tabMember = ref([])
+
+const countMember = computed(() => tabMember.value.length)
+
+async function getMemberInList() {
+  const { data: members } = await supabase.from('teams').select('members').eq('name', 'LesGoats')
+  members[0].members.forEach((member) => {
+    tabMember.value.push(member)
+  })
+}
 
 async function getTeamName() {
   const { data: teams } = await supabase.from('teams').select('name')
   teamName.value = teams[0].name
 }
 
+function addMember() {
+  tabMember.value.push(name.value)
+  name.value = ''
+}
+
+function deleteMember(member) {
+  tabMember.value.splice(tabMember.value.indexOf(member), 1)
+}
+
+async function saveTeam() {
+  const newMembers = Array()
+
+  tabMember.value.forEach((member) => {
+    newMembers.push(member)
+  })
+
+  const { data, error } = await supabase
+    .from('teams')
+    .update({ members: newMembers })
+    .eq('name', 'LesGoats')
+    .select()
+}
+
 getTeamName()
+getMemberInList()
 </script>
 
 <template>
@@ -22,15 +57,21 @@ getTeamName()
 
   <div class="flex flex-col">
     <h2 class="pb-9 pl-1 text-xl">Members</h2>
-    <div class="pb-5 pl-4" v-for="member in tabMember" :key="member">
-      <input type="text" class="border border-black mr-2" :value="member" />
+    <div class="pb-5 pl-4" v-if="countMember != 5">
+      <input type="text" class="border border-black mr-2" v-model="name" @keyup.enter="addMember" />
       <button
-        v-if="tabMember == null"
+        v-if="countMember < 5"
+        @click="addMember"
         class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800"
       >
         +
       </button>
+    </div>
+    <div class="pb-5 pl-4" v-for="member in tabMember" :key="member.value">
+      <input type="text" class="border border-black mr-2" :value="member" disabled />
       <button
+        v-if="countMember > 0"
+        @click="deleteMember(member)"
         class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800"
       >
         -
